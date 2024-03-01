826786ebf95b3d2e599efc7ae0381a081f468558
TriptiBisen23 <161804822+TriptiBisen23@users.noreply.github.com>
2024-03-01 23:28:17 +0530

    Add files via upload

---
+++ ques13.js
@@ -0,0 +1,11 @@
+// const input = require("readline-sync");
+
+// let n = input.questionInt("enter the number:");
+
+// if(2%n==0)
+// {
+//   console.log(n);
+// } else(2*n)
+// {
+//     console.log(n);
+// }
